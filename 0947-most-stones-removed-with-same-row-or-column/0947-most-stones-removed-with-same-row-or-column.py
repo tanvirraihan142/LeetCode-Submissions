cceb88917919f6ba36d1c6520c870bc0a93e7645
Raihanul Bari Tanvir <tanvir142@gmail.com>
2022-11-14 02:56:56 -0500

    Time: 9642 ms (5.04%), Space: 14.6 MB (72.25%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -5,35 +5,26 @@ class Solution:
         
         max_path = 0
         mapper = {}
-        
         visited = set()
         
         for i in range(len(stones)):
             starter = stones[i]
-            
             queue = deque()
             queue.appendleft([starter[0],starter[1],0])
-            
             visited.add(i)
             
             while queue:
-                #print(queue)
                 x,y,s = queue.popleft()             
                 for j in range(len(stones)):
                     
                     if stones[j][1] == y and stones[j][0] != x and j not in visited:
-                        #max_path = max(max_path,s+1)
                         max_path += 1
                         queue.appendleft([stones[j][0],y,s+1])
                         visited.add(j)
                         
                     if stones[j][0] == x and stones[j][1] != y and j not in visited:
-                        #max_path = max(max_path,s+1)
                         max_path += 1
                         queue.appendleft([x,stones[j][1],s+1])
                         visited.add(j)
                         
-            #print(visited)
-            
-            
         return max_path
