1ba77c3a2cfbcb125d95bc9a59cb868d26f8c72d
Raihanul Bari Tanvir <tanvir142@gmail.com>
2022-12-03 23:12:54 -0500

    Time: 53 ms (63.64%), Space: 13.8 MB (63.64%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        words = sentence.split(' ')
+        if len(words)==1:
+            if words[0][0] != words[0][-1]:
+                return False
+            else:
+                return True
+
+        else:
+            for i in range(1, len(words)):
+                if words[i-1][-1] != words[i][0]:
+                    return False
+            if words[0][0] != words[-1][-1]:
+                return False
+        return True
