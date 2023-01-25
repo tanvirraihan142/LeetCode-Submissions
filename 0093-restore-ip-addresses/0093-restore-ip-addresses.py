a1230dba2f77ae0bdcfb39c6be308013f98f9912
Raihanul Bari Tanvir <tanvir142@gmail.com>
2023-01-24 21:37:22 -0500

    Time: 69 ms (14.08%), Space: 13.9 MB (75.40%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        def is_valid_part(s):
+            return len(s)==1 or (s[0]!='0' and int(s) <= 255)
+        
+        result, parts = [], ['']*4
+        
+        for i in range(1,min(4,len(s))):
+            parts[0] = s[:i]
+            
+            if is_valid_part(parts[0]):
+                for j in range(1, min(len(s)-i,4)):
+                    parts[1] = s[i:i+j]
+                    
+                    if is_valid_part(parts[1]):
+                        for k in range(1, min(len(s)-i-j,4)):
+                            parts[2], parts[3] = s[i+j:i+j+k], s[i+j+k:]
+                            
+                            if is_valid_part(parts[2]) and is_valid_part(parts[3]):
+                                result.append('.'.join(parts))
+        return result
