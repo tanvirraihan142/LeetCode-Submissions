894968e701a7a0f5b045b9625b10cd0e9c6f9ff0
Raihanul Bari Tanvir <tanvir142@gmail.com>
2025-04-22 20:09:58 -0500

    Create file

---
+++ 189-RotateArray/189-RotateArray.py
@@ -0,0 +1,9 @@
+# Last updated: 4/22/2025, 8:09:57 PM
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[n-k:] + nums[:n-k]
