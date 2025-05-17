9131c094c7364b080491b3583f303244b425ce72
Raihanul Bari Tanvir <tanvir142@gmail.com>
2025-05-17 17:55:44 -0500

    File updated at 5/17/2025, 5:55:44 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,10 +1,18 @@
-# Last updated: 5/17/2025, 5:42:59 PM
+# Last updated: 5/17/2025, 5:55:44 PM
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        h = {}
-        for i, num in enumerate(nums):
-            n = target - num
-            if n not in h:
-                h[num] = i
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in set(['(','{','[']):
+                stack.append(c)
             else:
-                return [h[n], i]
+                if len(stack)>0 and ((c==']' and stack[-1]=='[') or \
+                 (c==')' and stack[-1] == '(') or \
+                  (c=='}' and stack[-1]=='{')):
+                    stack.pop()
+                else:
+                    return False
+        if not stack:        
+            return True
+        else:
+            return False
