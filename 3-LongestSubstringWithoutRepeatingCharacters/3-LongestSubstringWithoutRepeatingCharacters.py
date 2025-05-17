57f47251c7753ac0f30ad3ec1821fe5067257f8f
Raihanul Bari Tanvir <tanvir142@gmail.com>
2025-05-17 17:42:59 -0500

    File updated at 5/17/2025, 5:42:59 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,17 +1,10 @@
-# Last updated: 5/16/2025, 8:28:16 PM
+# Last updated: 5/17/2025, 5:42:59 PM
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        left, right = 0, 0
-        char_set = set()
-        max_length = 0
-
-        while right < len(s):
-            if s[right] not in char_set:
-                char_set.add(s[right])
-                max_length = max(max_length, right - left + 1)
-                right += 1
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
             else:
-                char_set.remove(s[left])
-                left += 1
-
-        return max_length
+                return [h[n], i]
