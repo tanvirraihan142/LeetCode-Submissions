9816f0f635f5c28d2c0ab2378985c4b8a69134ee
Raihanul Bari Tanvir <tanvir142@gmail.com>
2025-05-17 19:17:09 -0500

    Create file

---
+++ 100-SameTree/100-SameTree.py
@@ -0,0 +1,12 @@
+# Last updated: 5/17/2025, 7:17:09 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def convert(p):
+            return "^" + str(p.val) + "#" + convert(p.left) + convert(p.right) if p else "$"
+        return convert(p) == convert(q)
